243de2d504558fa73a7d828f1ea5260861b4a44d
Chet Corcos <ccorcos@gmail.com>
2015-10-09 23:58:48 -0700

    underscore string

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ccorcos:utils',
   summary: 'A patchwork of utilites missing from Underscore and Ramda',
-  version: '0.0.2',
+  version: '0.0.3',
   git: 'https://github.com/ccorcos/meteor-utils'
 });
 
@@ -9,6 +9,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2');
   var packages = [
     'underscore',
+    'underscorestring:underscore.string@3.2.2',
     'ramda:ramda@0.17.1',
     'random',
     'check',
